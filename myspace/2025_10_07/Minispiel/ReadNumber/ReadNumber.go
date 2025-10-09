ddabea84952c411d23237a15bac26861f97628f0
LukasNiedling <lukasniedling2006@gmail.com>
2025-10-09 14:27:37 +0200

    4

--- myspace/2025_10_07/Minispiel/ReadNumber/ReadNumber.go
+++ myspace/2025_10_07/Minispiel/ReadNumber/ReadNumber.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func ReadNumber(prompt string) int { 
 	var n int
-	fmt.Println("Gib eine Zahl ein um zu raten.")
+	fmt.Println("Gib eine Zahl zwischen 1 & 3 ein um zu raten.")
 	fmt.Scan(&n)
 	return n
 }
