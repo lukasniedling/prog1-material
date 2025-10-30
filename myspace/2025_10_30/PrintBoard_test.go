fdabc9f0a3ec8f959cb65db0a59b2b73d4f21d37
LukasNiedling <lukasniedling2006@gmail.com>
2025-10-30 16:54:33 +0100

    loll

---
+++ myspace/2025_10_30/PrintBoard_test.go
@@ -0,0 +1,12 @@
+package PrintBoard
+
+import "fmt"
+
+func PrintBoard(board [5][5]string) {
+	for _, row := range board {
+		for _, cell := range row {
+			fmt.Print(cell + " ")
+		}
+		fmt.Println()
+	}
+}
