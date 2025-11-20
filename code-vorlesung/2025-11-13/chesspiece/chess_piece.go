1184de5104fa6278397650e8d72373cb9bd104c3
LukasNiedling <lukasniedling2006@gmail.com>
2025-11-20 14:10:16 +0100

    hahahahahahhahahahahahahahahahahahah

--- code-vorlesung/2025-11-13/chesspiece/chess_piece.go
+++ code-vorlesung/2025-11-13/chesspiece/chess_piece.go
@@ -50,20 +50,71 @@ func (p ChessPiece) MoveAllowed(row, col int) bool {
 	// case BISHOP: ...
 	// case KNIGHT: ...
 	// }
+	if row < 7 && row >= 0 && col < 7 && col >= 0 {
 
-	if p.pieceType == BISHOP {
-		// Diagonale von links unten nach rechts oben.
-		if row-col == p.row-p.column {
-			return true
+		if p.pieceType == BISHOP {
+			// Diagonale von links unten nach rechts oben.
+			if row-col == p.row-p.column {
+				return true
+			}
+			// Diagonale von links oben nach rechts unten.
+			if row+col == p.row+p.column {
+				return true
+			}
 		}
-		// Diagonale von links oben nach rechts unten.
-		if row+col == p.row+p.column {
-			return true
+		if p.pieceType == KNIGHT {
+			// "L"-förmige Bewegungen
+			if (((row-p.row) == 2 || (row-p.row) == -2) && ((col-p.column) == 1 || (col-p.column) == -1)) ||
+				(((row-p.row) == 1 || (row-p.row) == -1) && ((col-p.column) == 2 || (col-p.column) == -2)) {
+				return true
+			}
 		}
-	}
-	if p.pieceType == KNIGHT {
-		/* ... */
-	}
 
+		if p.pieceType == ROOK {
+			// gleiche Reihe oder gleiche Spalte
+			if row == p.row || col == p.column {
+				return true
+			}
+		}
+
+		if p.pieceType == PAWN {
+			// Ein Feld vorwärts (ohne Berücksichtigung von Anfangs-Doppelschritt oder Schlagen)
+			if p.colour == WHITE && row == p.row-1 && col == p.column {
+				return true
+			}
+			if p.colour == BLACK && row == p.row+1 && col == p.column {
+				return true
+			}
+		}
+
+		if p.pieceType == KING {
+			// Ein Feld in jede Richtung.
+			// Prüft, ob der Reihen-Unterschied zwischen -1 und 1 liegt.
+			if (row-p.row) >= -1 && (row-p.row) <= 1 {
+				// UND ob der Spalten-Unterschied zwischen -1 und 1 liegt.
+				if (col-p.column) >= -1 && (col-p.column) <= 1 {
+					return true
+				}
+			}
+		}
+
+		if p.pieceType == QUEEN {
+			// Kombination aus Turm und Läufer
+			// Diagonale von links unten nach rechts oben.
+			if row-col == p.row-p.column {
+				return true
+			}
+			// Diagonale von links oben nach rechts unten.
+			if row+col == p.row+p.column {
+				return true
+			}
+			// gleiche Reihe oder gleiche Spalte
+			if row == p.row || col == p.column {
+				return true
+			}
+		}
+
+		
+	}
 	return false
 }
